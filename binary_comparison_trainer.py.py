18dce9d4e9ba12ced13f7b65b1bbc4dd7664c318
Revant Gupta <aron0093@gmail.com>
2018-01-17 08:55:48 +0100

    Added the binary_classifier of peptide pairs. See README for details.

---
+++ binary_comparison_trainer.py.py
@@ -0,0 +1,452 @@
+'''
+Author: Revant Gupta
+        SciLife Summer Fellow 2017
+        Lukas Kall Lab
+
+Description:
+
+* MS/MS analysis is routinely performed for the identification and quantification of proteins/peptides in biological samples.
+* The ionisation efficiency of peptides depends partly on experitmental conditions and on the peptide itself.
+* The quanitifcation of a peptide will in part depend on how efficiently it was ionised.
+* In this code we use preprocessed MS/MS quanitifcation data that uses a single value for each peptide which is derived from the quanitification in multiple runs.
+* For peptide with equal sample concentration (i.e. belonging to the same protein) we assume the higher value implies higher ionisation efficiency.
+* The code generates a trained model that can be used to create a ranked list of peptides by ionisation efficiency by sorting the binary comparisons.
+* The code can additionally output a trained autoencoder that converts peptide representations into 240 feature encodings.
+* The code can optionally be run using pre-trained models (autoencoder and classifier).
+
+'''
+# coding: utf-8
+
+# General Libraries
+
+import pandas as pd
+import numpy as np
+import random as ra
+import re
+import os
+import time
+import itertools
+import sys
+from sklearn.utils import shuffle
+
+# Machine Learning
+
+from keras.models import Sequential, Model
+from keras.layers.convolutional import Conv2D
+from keras.layers import Dense, Flatten, Merge, Dropout, Reshape
+from keras import optimizers as opt
+from keras import regularizers as reg
+from keras import initializers as init
+from keras.models import load_model
+
+# Arguments
+
+import argparse
+
+apars = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+apars.add_argument('-train', default='train.tsv',
+                       help='''Training data set''')
+
+apars.add_argument('-val', default=None,
+                       help='''Validation data set (Optional)''')
+
+apars.add_argument('-out', default = 'classifier.h5',
+                        help = ''' Save classifier weights after training''')
+
+apars.add_argument('-out_encoder', default = 'encoder.h5',
+                        help = ''' Save encoder weights after training''')
+
+apars.add_argument('-clas_epochs', default = 2000,
+                        help = ''' Number of training epochs for the classifier''')
+
+apars.add_argument('-encode_epochs', default = 25,
+                        help = ''' Number of training epochs for the autoencoder''')
+
+apars.add_argument('-batch', default = 500,
+                        help = ''' Batch size. Must be equal to or less than the number of data points in either set.''')
+
+apars.add_argument('-model', default = None,
+                        help = ''' Load existing classifier model for further training. (Optional)''')
+
+apars.add_argument('-encoder', default = None,
+                        help = ''' Load existing autoencoder model for encoding data. (Optional)''')
+
+apars.add_argument('-log', default = None,
+                        help = ''' Training logs. (Optional)''')
+
+args = apars.parse_args()
+
+clas_epochs = args.clas_epochs # Select
+encode_epochs = args.encode_epochs # Select
+batch_size = args.batch # Minimum number of data points required 
+
+  #--------------------------------------------------------------------------------------------------------------------------------------------#  
+   # Data processing scripts for raw MS data can be found in the processing folder of the repository. This script only allows a specific format.
+   #-------------------------------------------------------------------------------------------------------------------------------------------#
+
+# List of amino acids in peptides and replacing M[16] as X
+
+aa_alphabet= ['K', 'E', 'G', 'V', 'P', 'D', 'M', 'Y', 'T', 'A', 'R', 'F', 'N', 'Q', 'L', 'S', 'I','C', 'H', 'W', 'X']
+
+# Generate training logs
+
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open(str(args.log), "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass
+
+if args.log != None:    
+    sys.stdout = Logger()
+
+###################################################################################################################################################################################
+###################################################################################################################################################################################
+
+# Function to generate compared peptide pairs and training targets.
+
+def data_preprocessing(inp):
+
+    data = pd.read_csv(inp, sep = '\t')
+
+    cols = ['peptide', 'protein','value']
+
+    data = data[cols]
+
+    # Creating protein wise arrays for comparison
+
+    prot_info = []
+    io_eff = []
+
+    protein_list = list(set(data['protein']))
+
+    for i in range(len(protein_list)):
+
+        temp = data[data['protein'] == protein_list[i]]
+        temp.reset_index(drop = True, inplace = True)
+
+        info_array = []
+        io_array = []    
+        
+        for t, info in enumerate(temp['peptide']):
+
+            info_array.append(info)
+            io_array.append(float(temp['value'][t]))
+
+        prot_info.append(info_array)
+        io_eff.append(io_array)
+
+    del data # Free up space by removing data that is no longer needed
+
+    # Generating comparisons for training input
+
+    comparisons = []
+    reverse = []
+
+    drop_count = 0
+
+    for j, prot in enumerate(prot_info):
+  
+        comparison_number = len(prot)
+
+        if comparison_number == 1:
+            drop_count += 1
+            continue
+
+        # a = re.randint(0, comparison_number -1 )
+        # b = re.randint(0, comparison_number -1 ) # Random number of pairs equal to the number of peptides
+        
+        for a in range(comparison_number):
+
+            for b in range(a+1,comparison_number): # All possible pairs
+
+                assert a!=b
+                    
+                if float(io_eff[j][a])>float(io_eff[j][b]):
+
+                    comparisons.append([prot[a], prot[b]])
+                    reverse.append([prot[b], prot[a]])
+   
+                if float(io_eff[j][a])<float(io_eff[j][b]):
+
+                    comparisons.append([prot[b], prot[a]])
+                    reverse.append([prot[a], prot[b]])
+
+    del prot_info # Cleanup
+    del io_eff # Cleanup
+        
+    # Generating the information set 
+
+    target_len = len(comparisons)
+
+    comparisons.extend(reverse)           
+    
+    targets = [1]*(int(target_len))+[0]*(int(target_len))
+    
+    if drop_count > 0:    
+        
+        print("%d proteins were dropped as they only had one peptide."%(drop_count))
+
+    assert len(comparisons) == len(targets)
+
+    return comparisons, targets
+
+###################################################################################################################################################################################
+###################################################################################################################################################################################
+
+# Function to build peptide representations
+
+def pep_rep_builder(comparison):
+
+    # Function provides peptide representations for both peptides within one comparison
+
+    rep_list = []
+
+    for info in comparison:
+
+        peptide = info
+
+        rep = np.ones((1,120, len(aa_alphabet)), dtype=np.float)*0.0000000001
+        
+        for j in range(len(peptide)):
+
+            rep[0,60-len(peptide)+j, aa_alphabet.index(peptide[j])] = 1 # Peptide representation
+            rep[0,119-(60-len(peptide)+j), aa_alphabet.index(peptide[j])] = 1 # Mirrored representation
+            
+            rep_list.append(rep)
+
+    return rep_list
+
+# Infinite generator to yield inputs and targets (required for autoencoder fitting later)
+
+def auto_input_gen(inputs, batch_size):
+
+    # Shuffle data
+
+    indices = shuffle(list(range(len(inputs))), random_state= 0)
+
+    steps = list(range(int(len(inputs)/batch_size)+1))
+
+    for j in itertools.cycle(steps):
+
+        input_array_a = []
+        input_array_b = []
+        target_array = []
+    
+        for i in range(j,j+batch_size):
+
+            input_array_a.append(np.array(pep_rep_builder(inputs[indices[i]])[0]))
+            input_array_b.append(np.array(pep_rep_builder(inputs[indices[i]])[1]))
+            target_array.append(np.concatenate((np.array(pep_rep_builder(inputs[indices[i]])[0]), np.array(pep_rep_builder(inputs[indices[i]])[1])), axis = -2))
+                
+        yield ([np.array(input_array_a), np.array(input_array_b)], np.array(target_array))
+
+# Infinite generator to yield inputs (required for autoencoder output later)
+
+def auto_pred_gen(inputs, batch_size):
+
+    # Shuffle data
+
+    indices = list(range(len(inputs)))
+
+    steps = list(range(int(len(inputs)/batch_size)+1))
+
+    for j in itertools.cycle(steps):
+
+        input_array_a = []
+        input_array_b = []
+    
+        for i in range(j,j+batch_size):
+
+            input_array_a.append(np.array(pep_rep_builder(inputs[indices[i]])[0]))
+            input_array_b.append(np.array(pep_rep_builder(inputs[indices[i]])[1]))
+                
+        yield ([np.array(input_array_a), np.array(input_array_b)])
+
+
+###################################################################################################################################################################################
+###################################################################################################################################################################################
+
+# The autoencoer to generate 120 element encodings for the classifier.
+
+def Auto_CNN():
+
+    # Optimizer is Adam RMSprop with Nesterv Momentum
+
+    nadam = opt.Nadam(lr=0.015, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004)
+
+    # Optimizer is SGD with Nesterov Momemntum
+
+    #sgd = opt.SGD(lr = 0.15, momentum = 0.9, nesterov = True)    
+
+    # Convolutions of both kinds on each peptide representation
+
+    # First peptide branch
+
+    model_a = Sequential()
+    model_a.add(Conv2D(800,(6,len(aa_alphabet)), input_shape = (1,120,len(aa_alphabet)), strides=(1,1), padding='valid', 
+                        data_format='channels_first', activation='relu', use_bias=False, kernel_initializer='glorot_uniform'))
+    model_a.add(Flatten())
+
+    # Second peptide branch
+
+    model_b = Sequential()
+    model_b.add(Conv2D(800,(6,len(aa_alphabet)), input_shape = (1,120,len(aa_alphabet)), strides=(1,1), padding='valid', 
+                        data_format='channels_first', activation='relu', use_bias=False, kernel_initializer='glorot_uniform'))
+    model_b.add(Flatten())
+
+    # Final model
+
+    cnv = Merge([model_a, 
+                model_b, 
+                ], 
+                mode = 'concat') # Deprecated, needs replacement
+
+    # Auto-encoder
+
+    model = Sequential()
+
+    model.add(cnv)
+
+    model.add(Dense(240, activation='relu', use_bias=True, bias_initializer = 'random_normal', kernel_initializer='glorot_uniform')) # Encoder
+
+    model.add(Dense(5040, activation='relu', use_bias=True, bias_initializer = 'random_normal', kernel_initializer='glorot_uniform'))
+
+    model.add(Reshape((1,240,len(aa_alphabet)))) # Decoder
+
+    model.compile(optimizer= nadam, loss='mse', metrics = ['accuracy'])
+
+    encoder = Model(inputs=model.input, outputs=(model.layers[1]).output)
+
+    return model, encoder
+
+# Binary classifier is a deep neural network that is trained on the encoded peptide pairs.
+
+def clas_NN():
+
+    # Optimizer is Adam RMSprop with Nesterv Momentum
+
+    #nadam = opt.Nadam(lr=0.015, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004)
+
+    # Optimizer is SGD with Nesterov Momemntum
+
+    sgd = opt.SGD(lr = 0.0015, momentum = 0.9, nesterov = True)    
+
+    model = Sequential()
+
+    model.add(Dense(240, activation='relu', use_bias=True, bias_initializer = 'random_normal', kernel_initializer='glorot_uniform', input_shape = (240,)))
+
+    for i in range(8):
+
+        model.add(Dense(240, activation='relu', use_bias=True, bias_initializer = 'random_normal', kernel_initializer='glorot_uniform'))
+        model.add(Dropout(0.5))
+
+    model.add(Dense(240, activation='relu', use_bias=True, bias_initializer = 'zeros', kernel_initializer='glorot_uniform'))
+    model.add(Dense(1, activation='sigmoid', use_bias=True, bias_initializer = 'zeros', kernel_initializer='glorot_uniform'))
+
+    model.compile(optimizer= sgd, loss='binary_crossentropy', metrics = ['binary_accuracy'])
+
+    return model
+
+###################################################################################################################################################################################
+###################################################################################################################################################################################
+
+
+start = time.time()
+
+print('\n')
+print('Processing data...')
+print('\n')
+
+train_inp, train_tar = data_preprocessing(args.train)
+
+if args.val != None:
+
+    val_inp, val_tar = data_preprocessing(str(args.val))
+
+print('\n')
+print('Running autoencoder...')
+print('\n')
+
+if args.encoder != None:
+
+    encoder = load_model(str(args.encoder))
+
+else:
+
+    model, encoder = Auto_CNN()
+
+    if args.val != None:
+
+        #model.fit_generator(auto_input_gen(train_inp, batch_size), (int(len(train_inp)/batch_size)+1), epochs=encode_epochs, verbose = 2, 
+        #                validation_data=auto_input_gen(val_inp, batch_size), validation_steps=(int(len(val_inp)/batch_size)+1),               
+        #                max_q_size=1, workers=4, pickle_safe=True, initial_epoch=0)
+
+        # Including validation data for autoencoder for correct encoding
+
+        enc_inp = train_inp + val_inp
+
+        model.fit_generator(auto_input_gen(enc_inp, batch_size), (int(len(enc_inp)/batch_size)+1), epochs=encode_epochs, verbose = 2, 
+                                       
+                        max_q_size=1, workers=4, pickle_safe=True, initial_epoch=0)
+
+    else:
+
+        model.fit_generator(auto_input_gen(train_inp, batch_size), (int(len(train_inp)/batch_size)+1), epochs=encode_epochs, verbose = 2, 
+                        max_q_size=1, workers=4, pickle_safe=True, initial_epoch=0)
+
+    encoder.save(args.out_encoder)
+
+print('\n')
+print('Generating encoded data...')
+print('\n')
+
+enc_data = pd.DataFrame()
+
+train_X = encoder.predict_generator(auto_pred_gen(train_inp, batch_size), (int(len(train_inp)/batch_size)+1), max_q_size=1, workers=4, pickle_safe=True)
+train_Y = train_tar+train_tar[:int(len(train_X)-len(train_tar))]
+
+if args.val != None:
+   
+    val_X = encoder.predict_generator(auto_pred_gen(val_inp, batch_size), (int(len(val_inp)/batch_size)+1), max_q_size=1, workers=4, pickle_safe=True)
+    val_Y = val_tar+val_tar[:int(len(val_X)-len(val_tar))]
+
+
+print('\n')
+print('Training classifier...')
+print('\n')
+
+if args.model != None:
+
+    classiier = load_model(str(args.model)) # Train an existing model
+
+else:
+
+    classifier = clas_NN()
+
+
+if args.val != None:
+
+    classifier.fit(train_X, train_Y, batch_size = 2000, epochs = clas_epochs, verbose = 2, validation_data = (val_X, val_Y))
+
+else:
+
+    classifier.fit(train_X, train_Y, batch_size = 2000, epochs = clas_epochs, verbose = 2)
+
+classifier.save(args.out)
+
+print('\n')
+
+print('The script took %.2f seconds to run'%(time.time()-start))
+
+###################################################################################################################################################################################
+###################################################################################################################################################################################
